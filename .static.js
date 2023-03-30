a6fecb76774743b3c4d6520de37aaa070bdc036d
David Herman <david.herman@gmail.com>
2023-03-29 19:49:53 -0700

    v0.0.4

---
+++ .static.js
@@ -0,0 +1,14 @@
+// AUTOMATICALLY GENERATED FILE. DO NOT EDIT.
+//
+// This code is never executed but is detected by the static analysis of
+// bundlers such as `@vercel/ncc`. The require() expression that selects
+// the right binary module for the current platform is too dynamic to be
+// analyzable by bundler analyses, so this module provides an exhaustive
+// static list for those analyses.
+
+return;
+
+require('@neon-prebuild-example/win32-x64-msvc');
+require('@neon-prebuild-example/darwin-x64');
+require('@neon-prebuild-example/linux-x64-gnu');
+require('@neon-prebuild-example/linux-x64-musl');
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { loadDebug, getTargetName } = require('./loader');
-require('./targets');
 
-module.exports = loadDebug(__dirname)
-  || require(`@neon-prebuild-example/${getTargetName()}`);
+// Dynamic require for runtime.
+module.exports = loadDebug(__dirname) || require(`@neon-prebuild-example/${getTargetName()}`);
+
+// Static requires for bundlers.
+if (0) require('./.static');
