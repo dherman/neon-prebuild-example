e79bb0f30dc63766a5f9c47355aeb7ed14782d42
David Herman <david.herman@gmail.com>
2023-04-17 21:24:35 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { debug, currentTarget } = require('@neon-rs/load');
+const { load, currentTarget } = require('@neon-rs/load');
 
 // Static requires for bundlers.
 if (0) { require('./.targets'); }
 
 // Dynamic require for runtime.
-module.exports = debug(__dirname) || require(`@neon-prebuild-example/${currentTarget()}`);
+module.exports = load(__dirname) || require(`@neon-prebuild-example/${currentTarget()}`);
